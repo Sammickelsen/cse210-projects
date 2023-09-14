3033518967b7da6368cf0b43db0506ee29922f4f
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-09-14 15:53:22 -0600

    Letter Grade Program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Ask user for a grade percentage
+        Console.Write("Please give your grade percentage for the class: ");
+        string textValue = Console.ReadLine();
+        int score = int.Parse(textValue);
+
+        // Compare percentage to letter grades
+        string letter;
+
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        else if (score >= 80)
+        {
+            letter = "B";
+        }
+        else if (score >= 70)
+        {
+            letter = "C";
+        }
+        else if (score >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine(letter);
+
+        if (score >= 70)
+        {
+            Console.WriteLine("Congradulations on passing the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you failed the class...");
+        }
+
+        
     }
 }
