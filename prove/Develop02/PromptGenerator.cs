80faafac4069f656e5a7b033bc9a8b7527c4879e
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-10-02 20:02:46 -0600

    Created GeneratePrompt method

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,12 +1,24 @@
+using System;
+using System.Collections.Generic;
 public class PromptGenerator
 {
     public PromptGenerator()
     {
-
     }
 
-    public void GeneratePrompt()
+    public string GeneratePrompt()
     {
+        // Create a string of possible prompts.
+        List<string> promptsList = new List<string>{"What was one good thing that happened today?", 
+        "If I had one thing I could do over today, what would it be?", 
+        "Who made me happiest today?"};
+
+        // Get a random prompt from the list by getting a random index.
+        var random = new Random();
+        int index = random.Next(promptsList.Count);
+        string randomPrompt = promptsList[index];
         
+        // Return the prompt to the AddEntry method.
+        return randomPrompt;
     }
 }
