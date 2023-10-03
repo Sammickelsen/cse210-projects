c67d7c824e69628ae582f4543f541ffc9e50e0ce
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-10-03 14:20:39 -0600

    Added additional prompts

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -11,7 +11,10 @@ public class PromptGenerator
         // Create a string of possible prompts.
         List<string> promptsList = new List<string>{"What was one good thing that happened today?", 
         "If I had one thing I could do over today, what would it be?", 
-        "Who made me happiest today?"};
+        "Who made me happiest today?",
+        "Where did you go today? What did you do there?",
+        "On a scale from 1-10, how good was your day? Explain your reasoning.",
+        "Did you see the hand of the Lord today? If so, where?"};
 
         // Get a random prompt from the list by getting a random index.
         var random = new Random();
