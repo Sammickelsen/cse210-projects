a3ce0b690c954f1b37a873a20e8b212fbfb46017
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-10-17 19:01:33 -0600

    Working on the Reference Class

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,30 +1,39 @@
 using System;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        bool continueLoop = true;
-
-        //Create Scripture list made of Word objects for each word.
-        List<Word> wordList = new List<Word>();
+        //Create scripture and reference variables to pull from.
         string scripRef =  "Ether 12:27";
         string scripContent = "And if men come unto me I will show unto them their weakness." +
       " I give unto men weakness that they may be humble; and my grace is sufficient for all men that humble themselves before me;" +
       " for if they humble themselves before me, and have faith in me, then will I make weak things become strong unto them.";
+        
+        //Create Scripture List
+        List<Word> wordList = new List<Word>();
         Array scripList = scripContent.Split(' ');
-
         foreach (string word in scripList)
         {
             Word newWord = new Word(word);
             wordList.Add(newWord);
         }
 
-       Scripture userScripture = new Scripture(scripRef, wordList);
+        //Create Reference
+        var referenceList = scripRef.Split(' ', ':', '-');
+        Reference userReference = new Reference(referenceList[0], referenceList[1], referenceList[2]);
+
+        // Create Scripture object.
+        Scripture userScripture = new Scripture(userReference.CreateReference(), wordList);
+
+
 
         //Set the basic program loop.
+        bool continueLoop = true;
         do
         {
             Console.Clear();
