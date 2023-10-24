843fd95ae97b7c5563cefd628b145e4f738ab4c4
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-10-24 08:03:07 -0600

    Initial Commit

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment assignment = new MathAssignment("Samuel Mickelsen", "Calculous", "7.3", "8-19");
+        string summary = assignment.GetSummary();
+        string homeworkList = assignment.GetHomeworkList();
+        Console.WriteLine(summary);
+        Console.WriteLine(homeworkList);
+
+        Console.WriteLine();
+
+        WritingAssignment english = new WritingAssignment("Samuel Mickelsen", "History", "Art through the Ages");
+        string englishSummary = english.GetSummary();
+        string writingInformation = english.GetWritingInformation();
+        Console.WriteLine(englishSummary);
+        Console.WriteLine(writingInformation);
     }
 }
