2a0168180406b730f00ca9ec598101de9c52cebc
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-10-30 12:52:27 -0600

    Initial Commit

---
+++ prove/Develop04/Reflect.cs
@@ -0,0 +1,14 @@
+public class Reflect
+{
+    private List<string> _promptList;
+    private List<string> _questionList;
+    private string _randPrompt;
+    private string _randQuestion;
+
+    public Reflect(List<string> prompts, List<string> questions, string name, string desc, string duration) : base Activity(string name, string desc, string duration)
+    {
+        _promptList = prompts;
+        _questionList = questions;
+
+    }
+}
