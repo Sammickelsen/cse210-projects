62467e191ce7db9346084029c34e6216484c4944
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-10-30 12:44:48 -0600

    Initial Commit

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,29 @@
+public class Activity
+{
+    private string _activityName;
+    private string _activityDescription;
+    private int _activityDuration;
+    public int Count;
+
+    public Activity(string name, string desc, int duration)
+    {
+        _activityName = name;
+        _activityDescription = desc;
+        _activityDuration = duration;
+    }
+
+    public void EndMessage()
+    {
+
+    }
+
+    public void StartMessage()
+    {
+
+    }
+
+    public void Countdown(int count)
+    {
+
+    }
+}
