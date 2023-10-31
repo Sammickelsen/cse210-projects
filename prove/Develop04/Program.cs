e639f43d0ab6b36170603534b70c09e1b009ab32
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-10-31 09:17:20 -0600

    Set up program loop

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        bool loopControl = true;
+
+        while (loopControl = true)
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start Breathing Activity");
+            Console.WriteLine("  2. Start Reflecting Activity");
+            Console.WriteLine("  3. Start Listing Activity");
+            Console.WriteLine("  4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            int response = Console.ReadLine();
+
+            if (response = 1)
+            {
+                Console.WriteLine("Breathe Activity");
+            }
+            else if (response = 2)
+            {
+                Console.WriteLine("Reflecting Activity");
+            }
+            else if (response = 3)
+            {
+                Console.WriteLine("Listing Activity");
+            }
+            else if (response = 4)
+            {
+                Console.WriteLine("Quitting...");
+                loopControl = false;
+            }
+            else
+            {
+                Console.WriteLine("Please input a valid answer (1-4)");
+            }
+        }
     }
 }
