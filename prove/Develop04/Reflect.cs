f7592ae0c032a202f0bb103c3ee291fe678ef252
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-10-31 16:31:39 -0600

    Reflect.cs has all information working

--- prove/Develop04/Reflect.cs
+++ prove/Develop04/Reflect.cs
@@ -1,7 +1,18 @@
 public class Reflect : Activity
 {
-    private string[] _promptList = {"Think about a time that you have helped someone.", "Think of a time you have felt happy."};
-    private string[] _questionList  = {""};
+    private string[] _promptList = {"Think of a time when you stood up for someone else.",
+     "Think of a time when you did something really difficult.",
+     "Think of a time when you helped someone in need.",
+      "Think of a time when you did something truly selfless."};
+    private string[] _questionList  = {"Why was this experience meaningful to you?",
+    "Have you ever done anything like this before?",
+    "How did you get started?",
+    "How did you feel when it was complete?",
+    "What made this time different than other times when you were not as successful?",
+    "What is your favorite thing about this experience?",
+    "What could you learn from this experience that applies to other situations?",
+    "What did you learn about yourself through this experience?",
+    "How can you keep this experience in mind in the future?"};
     private string _randPrompt;
     private string _randQuestion;
 
@@ -28,9 +39,11 @@ public class Reflect : Activity
 
     public void DisplayQuestion()
     {
+        Console.WriteLine(_randPrompt);
     }
 
     public void DisplayPrompt()
     {
+        Console.WriteLine(_randQuestion);
     }
 }
