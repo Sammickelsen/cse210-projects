a56b77367bf8d323e8c73263287df4b0faa99264
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-10-31 08:36:57 -0600

    Initial Commit

---
+++ prove/Develop04/Breathe.cs
@@ -0,0 +1,12 @@
+public class Breathe
+{
+    public Breathe(string name, string desc, string duration) : base Activity(name, desc, duration)
+    {
+
+    }
+
+    public void InOutLoop(int _activityDuration)
+    {
+
+    }
+}
