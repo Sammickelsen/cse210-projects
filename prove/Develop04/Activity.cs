7ea68ac37065dbf47b7fca8379e38506e60552d9
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-10-31 09:03:04 -0600

    Almost finished setting up

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -13,7 +13,9 @@ public class Activity
 
     public void EndMessage()
     {
-
+        Console.WriteLine("Well Done!");
+        Consoel.WriteLine();
+        Console.WriteLine($"You have completed another {_activityDuration} seconds of the {_activityName} Activity.");
     }
 
     public void StartMessage()
@@ -21,10 +23,13 @@ public class Activity
         Console.Clear();
         Console.WriteLine($"Welcome to the {_activityName} Activity");
         Console.WriteLine();
+
         Console.WriteLine(_activityDescription);
         Console.WriteLine();
+
         Console.Write("How long, in seconds, would you like for your session?");
         _activityDuration = Console.ReadLine();
+        Console.WriteLine();
     }
 
     public void Countdown(int count)
