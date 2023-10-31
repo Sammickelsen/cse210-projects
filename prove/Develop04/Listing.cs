db1e70bcf15cbb17d909f9217d8b4471f52b4847
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-10-31 08:34:30 -0600

    Initial Commit

---
+++ prove/Develop04/Listing.cs
@@ -0,0 +1,31 @@
+public class Listing
+{
+    private string _prompt;
+    private List<string> _prompts;
+    private List<string> _userList;
+
+    public Listing(List<string> prompts, string name, string desc, string duration) : base Activity(name, desc, duration)
+    {
+        _prompts = prompts
+    }
+
+    public string GetPrompt()
+    {
+
+    }
+
+    public string DisplayPrompt()
+    {
+
+    }
+
+    public string RecordListing()
+    {
+
+    }
+
+    public void DisplayList()
+    {
+
+    }
+}
