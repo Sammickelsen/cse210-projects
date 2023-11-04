6696008891e16b9ca4f025b3cf6bac61916a5bdd
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-11-04 16:16:09 -0600

    Initial Commit

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("blue", 5);
+        Rectangle rectangle = new Rectangle("red", 2, 6);
+        Circle circle = new Circle("yellow", 4);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine();
+        }
     }
 }
