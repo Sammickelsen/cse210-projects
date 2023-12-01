2d37add4ab8a6973a5817213a6b153e7a2f1afb6
Samuel Mickelsen <samuel.d.mickelsen@gmail.com>
2023-12-01 12:59:39 -0700

    Starting Work on Writing Files

--- final/FinalProject/ReadWrite.cs
+++ final/FinalProject/ReadWrite.cs
@@ -1,11 +1,24 @@
+using System.IO;
+
 public class ReadWrite
 {
     public ReadWrite()
     {
     }
 
-    public void WriteFiles()
+    public void WriteFiles(List<Day> week, List<Breakfast> breakfasts, List<Lunch> lunch, List<Dinner> dinner)
     {
+        Console.Write("Please name the file you want to save to (Without .txt): ");
+        string fileBase = Console.ReadLine();
+
+        using (StreamWriter outputFile = new StreamWriter(fileBase + "Calendar.txt"))
+        {
+            foreach (Day d in week)
+            {
+                string outPutString = $"{d.dayName}|{d.GetBreakfast},{d.GetLunch},{d.GetDinner}";
+            }
+        }
+
 
     }
 
